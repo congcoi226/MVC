f761829cf51c58c86708d7343e9f6d451541a23c
congcoi226 <kevilkoi2@gmail.com>
2018-08-09 09:51:36 +0700

    b1/1_reduce_code_in_views.py

--- manage.py
+++ manage.py
@@ -1,15 +1,10 @@
 
 from views.views import get_subject_with_class_id
-from views.views import get_teacher_with_subject_id
+from views.views import get_subject_with_teacher_id
 from views.views import get_student_with_subject_id
 
 
 if __name__ == '__main__':
-    
-    get_subject_with_class_id()
-
-if __name__ =='__main__':
-	get_teacher_with_subject_id()
-
-if __name__ == '__main__':
+	get_subject_with_class_id()
+	get_subject_with_teacher_id()
 	get_student_with_subject_id()
--- views/views.py
+++ views/views.py
@@ -1,55 +1,54 @@
 from controllers.controllers import Controllers
 
 
-
+def get_input():
+	for i in range(2):
+		try:
+			input_id = int(input('Please input id: '))
+			return input_id
+		except (KeyError,ValueError):
+			continue
+	return False
 
 
 def get_subject_with_class_id():
-    for i in range(2):
-        try:
-            class_id = int(input('Input your class id: '))
-            classes,subjects = Controllers().get_classes_and_subject_with_class_id(class_id)
-            if subjects:
-                for each_subject in subjects:
-                    print('Name Subject: ',each_subject.name)
-            else:
-                print('Not fine this class id')
-            return True
-        except (KeyError,ValueError):
-            print('Your input is not integer. Please input again')
-            continue
-    print('You can not get teachers anymore')
-    return False
+	print('Get subject with class id')
+	class_id = get_input()
+	if class_id:
+		classes,subjects = Controllers().get_classes_and_subject_with_class_id(class_id)
+	else:
+		return False
+	if subjects:
+		for each_subject in subjects:
+			print('Name Subject: ',each_subject.name)
+	else:
+		print('Not fine this class id')
+	return True
 
-def get_teacher_with_subject_id():
-    for i in range(2):
-        try:
-            teacher_id = int(input('Input your teacher id: '))
-            teachers,subjects = Controllers().get_teachers_and_subjects_with_teacher_id(teacher_id)
-            if subjects:
-                for each_subject in subjects:
-                    print('Name Subject: ',each_subject.name)
-            else:
-                print('Not fine this teacher id')
-            return True
-        except (KeyError,ValueError):
-            print('Your input is not integer. Please input again')
-            continue
-    print('You can not get teachers anymore')
-    return False
+def get_subject_with_teacher_id():
+	print('Get subject with teacher id')
+	teacher_id = get_input()
+	if teacher_id:
+		teachers,subjects = Controllers().get_teachers_and_subjects_with_teacher_id(teacher_id)
+	else:
+		return False
+	if subjects:
+		for each_subject in subjects:
+			print('Name Subject: ',each_subject.name)
+	else:
+		print('Not fine this teacher id')
+	return True
+
 def get_student_with_subject_id():
-    for i in range(2):
-        try:
-            subject_id = int(input('Input your subject id: '))
-            students,subjects = Controllers().get_subject_and_student_with_subject_id(subject_id)
-            if subjects:
-                for each_subject in subjects:
-                    print('List Student: ',each_subject.name)
-            else:
-                print('Not fine this subject id')
-            return True
-        except (KeyError,ValueError):
-            print('Your input is not integer. Please input again')
-            continue
-    print('You can not get teachers anymore')
-    return False
+	print('Get student with subject id')
+	subject_id = get_input()
+	if subject_id:
+		students,subjects = Controllers().get_subject_and_student_with_subject_id(subject_id)
+	else:
+		return False
+	if subjects:
+		for each_subject in subjects:
+			print('List Student: ',each_subject.name)
+	else:
+		print('Not fine this subject id')
+	return True
